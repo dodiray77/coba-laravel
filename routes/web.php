7635c806ab55c46b8f885966c22d9187f9e2e337
dodiray <dodialfariziray@gmail.com>
2023-03-07 02:30:09 +0700

    materi episode 5 Laravel WPU

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,56 +30,11 @@ Route::get('/about', function () {
 
 
 
-Route::get('/blog', function () {
-    $blogPost = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Dodi",
-            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo iure reiciendis architecto sapiente rem voluptatum. Fugit, ipsam harum saepe quibusdam quo ratione soluta suscipit voluptatem maiores ipsum eaque ut corporis aspernatur magnam sapiente expedita sint, eveniet velit! Velit eum doloremque itaque aspernatur officia possimus unde magnam quibusdam fuga! Aliquam provident quo placeat optio quae tempore officiis vitae doloremque aperiam, natus neque illum ab sequi! Deleniti quo voluptas, facilis amet tempore illo excepturi numquam tenetur, esse ullam consequatur voluptatem nihil suscipit!"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Dodi",
-            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo iure reiciendis architecto sapiente rem voluptatum. Fugit, ipsam harum saepe quibusdam quo ratione soluta suscipit voluptatem maiores ipsum eaque ut corporis aspernatur magnam sapiente expedita sint, eveniet velit! Velit eum doloremque itaque aspernatur officia possimus unde magnam quibusdam fuga! Aliquam provident quo placeat optio quae tempore officiis vitae doloremque aperiam, natus neque illum ab sequi! Deleniti quo voluptas, facilis amet tempore illo excepturi numquam tenetur, esse ullam consequatur voluptatem nihil suscipit!,eveniet velit! Velit eum doloremque itaque aspernatur officia possimus unde magnam quibusdam fuga! Aliquam provident quo placeat optio quae tempore officiis vitae doloremque aperiam, natus neque illum ab sequi! Deleniti quo voluptas, facilis amet tempore illo excepturi numquam tenetur, esse ullam consequatur voluptatem nihil suscipit"
-        ]
-    ];
-    return view('blog', [
-        "title" => 'Blog',
-        "posts" => $blogPost
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
 
 // halaman single post
 
-Route::get('post/{slug}', function ($slug) {
-    $blogPost = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Dodi",
-            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo iure reiciendis architecto sapiente rem voluptatum. Fugit, ipsam harum saepe quibusdam quo ratione soluta suscipit voluptatem maiores ipsum eaque ut corporis aspernatur magnam sapiente expedita sint, eveniet velit! Velit eum doloremque itaque aspernatur officia possimus unde magnam quibusdam fuga! Aliquam provident quo placeat optio quae tempore officiis vitae doloremque aperiam, natus neque illum ab sequi! Deleniti quo voluptas, facilis amet tempore illo excepturi numquam tenetur, esse ullam consequatur voluptatem nihil suscipit!"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Dodi",
-            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo iure reiciendis architecto sapiente rem voluptatum. Fugit, ipsam harum saepe quibusdam quo ratione soluta suscipit voluptatem maiores ipsum eaque ut corporis aspernatur magnam sapiente expedita sint, eveniet velit! Velit eum doloremque itaque aspernatur officia possimus unde magnam quibusdam fuga! Aliquam provident quo placeat optio quae tempore officiis vitae doloremque aperiam, natus neque illum ab sequi! Deleniti quo voluptas, facilis amet tempore illo excepturi numquam tenetur, esse ullam consequatur voluptatem nihil suscipit!,eveniet velit! Velit eum doloremque itaque aspernatur officia possimus unde magnam quibusdam fuga! Aliquam provident quo placeat optio quae tempore officiis vitae doloremque aperiam, natus neque illum ab sequi! Deleniti quo voluptas, facilis amet tempore illo excepturi numquam tenetur, esse ullam consequatur voluptatem nihil suscipit"
-        ]
-    ];
-
-    foreach ($blogPost as $post) {
-        if ($post["slug"] == $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('post/{slug}', [PostController::class, 'tampil']);
 
 Route::get('/coba', function () {
     return view('coba', ["title" => 'Coba']);

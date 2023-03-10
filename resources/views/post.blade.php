61717210964b5ec44242a359112ff40cc4d2164c
dodiray <dodialfariziray@gmail.com>
2023-03-11 02:33:57 +0700

    fix bug on episode 10

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,14 +1,13 @@
 @extends('layouts.main')
 
 @section('container')
-    <article>
-        <H2>{{ $post->title }}</H2>
-        <p class="text-decoration-none">By: <a href="">{{ $post->user->name }}</a> in <a
-                href="/categories/{{ $post->category->slug }}">{{ $post->Category->name }}</a>
-        </p>
-        {!! $post->body !!}
-        <hr>
+<article>
+    <H2>{{ $post->title }}</H2>
+    <p class="text-decoration-none">By: <a href="">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->Category->name }}</a>
+    </p>
+    {!! $post->body !!}
+    <hr>
 
-        <a name="" id="" class="btn btn-primary" href="/blog" role="button">Back to Post</a>
-    </article>
+    <a name="" id="" class="btn btn-primary" href="/blog" role="button">Back to Post</a>
+</article>
 @endsection

e7c5ada37837ab2b489e94bf0ecc9641544418d9
dodiray <dodialfariziray@gmail.com>
2023-03-18 02:55:28 +0700

    Episode 15

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -12,5 +14,21 @@ class RegisterController extends Controller
             'title' => 'Register',
             'active' => 'register'
         ]);
+    }
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:35',
+            'username' => ['required', 'min:4', 'max:30', 'unique:users'],
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:8|max:255'
+        ]);
+
+        // $validatedData['password'] = bcrypt($validatedData['password']);
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        User::create($validatedData);
+        // $request->session()->flash('success', 'Registration successfull! Please login');
+        return redirect('/login')->with('success', 'Registration successfull! Please login');
     }
 }

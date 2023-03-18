fa1244febfaa0007b573c0a11e0817d290e3f635
dodiray <dodialfariziray@gmail.com>
2023-03-19 02:58:39 +0700

    episode 15

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -19,10 +19,30 @@
                     <a class="nav-link {{ ($active === 'categories') ? 'active' : '' }}" href="/categories">Categories</a>
                 </li>
             </ul>
+
             <ul class="navbar-nav ms-auto">
+                @auth
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Welcome back, {{ auth()->user()->name }}
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-layout-text-sidebar"></i> My Dashboard</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <form action="/logout" method="post">
+                            @csrf
+                            <button type="submit" class="dropdown-item"><i class="bi bi-door-closed-fill"></i> Logout</button>
+                        </form>
+                        <li></li>
+                    </ul>
+                </li>
+                @else
                 <li class="nav-item {{ ($active === 'login') ? 'active' : '' }}"">
                     <a href=" /login" class="nav-link"><i class="bi bi-door-open"></i>Login</a>
                 </li>
+                @endauth
             </ul>
         </div>
     </div>
